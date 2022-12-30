b77607b8b2573daf79957399343c55682491b73f
Zafir <zafirhasan2006@gmail.com>
2022-12-30 19:20:49 +0400

    Create renderBoard function and test it
    The renderBoard function updates the board's look based on where it's
    hit and where it has a ship. Tested it in a pretty messy way though will
    fix it later.

--- main.js
+++ main.js
@@ -10,6 +10,26 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
+/***/ "./src/game.js":
+/*!*********************!*\
+  !*** ./src/game.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"game\": () => (/* binding */ game)\n/* harmony export */ });\n/* harmony import */ var _player__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./player */ \"./src/player.js\");\n\n\nconst game = function() {\n    let user = (0,_player__WEBPACK_IMPORTED_MODULE_0__.player)();\n    let enemy = (0,_player__WEBPACK_IMPORTED_MODULE_0__.player)();\n\n    enemy.ownGameboard.placeShip([3, 4], 3, true);\n    user.attack([3, 4], enemy);\n\n    function playRound(userCordChoice) {\n        \n    }\n\n    function getUser() {\n        return user;\n    }\n\n    function getEnemy() {\n        return enemy;\n    }\n\n    return {\n        playRound,\n        getEnemy,\n        getUser\n    }\n}();\n\n\n//# sourceURL=webpack:///./src/game.js?");
+
+/***/ }),
+
+/***/ "./src/gameboard.js":
+/*!**************************!*\
+  !*** ./src/gameboard.js ***!
+  \**************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"gameboard\": () => (/* binding */ gameboard)\n/* harmony export */ });\n/* harmony import */ var _ship__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./ship */ \"./src/ship.js\");\n\n\nconst gameboard = function() {\n\n    let board = [];\n    let ships = [];\n\n    for (let x = 0; x < 10; ++x) {\n        let row = [];\n        for (let y = 0; y < 10; ++y) {\n            row.push({containsShip: -1, beenHit: false});\n        }\n        board.push(row);\n    }\n\n    function placeShip(start, length, isHorizontal) {\n        let shipToPlace = (0,_ship__WEBPACK_IMPORTED_MODULE_0__.ship)(length);\n        ships.push(shipToPlace);\n        if (isHorizontal) {\n            for (let i = start[1]; i < start[1] + length; ++i) {\n                board[start[0]][i].containsShip = ships.length - 1;\n            }\n        } else {\n            for (let i = start[0]; i < start[0] + length; ++i) {\n                board[i][start[1]].containsShip = ships.length - 1;\n            }\n        }\n    }\n    \n    function receiveAttack(coordinate) {\n        board[coordinate[0]][coordinate[1]].beenHit = true;\n    }\n\n    function hasBeenAttacked(coordinate) {\n        return (board[coordinate[0]][coordinate[1]].beenHit);\n    }\n\n    function hasShip(coordinate) {\n        return (board[coordinate[0]][coordinate[1]].containsShip);\n    }\n\n    function getBoard() {\n        return board;\n    }\n\n    function isOutOfBounds(start, length, isHorizontal) {\n        if (isHorizontal) {\n            return start[1] + length > 9;\n        }\n        return start[0] + length > 9;\n    }\n\n    function willCollide(start, length, isHorizontal) {\n        if (isHorizontal) {\n            for (let i = start[1]; i < start[1] + length; ++i) {\n                if (board[start[0]][i].containsShip != -1)\n                    return true;\n            }\n        } else {\n            for (let i = start[0]; i < start[0] + length; ++i) {\n                if (board[i][start[1]].containsShip != -1)\n                    return true;\n            }\n        }\n        return false;\n    }\n\n    function allShipsSunk() {\n        for (const shipToCheck of ships) {\n            if (!shipToCheck.isSunk())\n                return false;\n        }\n        return true;\n    }\n\n    return {\n        placeShip,\n        receiveAttack,\n        hasBeenAttacked,\n        getBoard,\n        isOutOfBounds,\n        willCollide,\n        hasShip\n    }\n}\n\n//# sourceURL=webpack:///./src/gameboard.js?");
+
+/***/ }),
+
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
@@ -20,13 +40,33 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _ui_
 
 /***/ }),
 
+/***/ "./src/player.js":
+/*!***********************!*\
+  !*** ./src/player.js ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"player\": () => (/* binding */ player)\n/* harmony export */ });\n/* harmony import */ var _gameboard__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./gameboard */ \"./src/gameboard.js\");\n\n\nconst player = function() {\n    const ownGameboard = (0,_gameboard__WEBPACK_IMPORTED_MODULE_0__.gameboard)();\n\n    function attack(cord, playerToAttack) {\n        playerToAttack.ownGameboard.receiveAttack(cord);\n    }\n\n    function randomAttack(playerToAttack) {\n        let cordX;\n        let cordY;\n        do {\n            cordX = Math.floor(Math.random() * 10);\n            cordY = Math.floor(Math.random() * 10);\n        } while (playerToAttack.ownGameboard.hasBeenAttacked([cordX, cordY]));\n        playerToAttack.ownGameboard.receiveAttack([cordX, cordY]);\n    }\n\n    return {\n        ownGameboard,\n        attack,\n        randomAttack\n    }\n}\n\n//# sourceURL=webpack:///./src/player.js?");
+
+/***/ }),
+
+/***/ "./src/ship.js":
+/*!*********************!*\
+  !*** ./src/ship.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"ship\": () => (/* binding */ ship)\n/* harmony export */ });\nconst ship = function(length) {\n\n    let hitCount = 0;\n    function hit() {\n        ++hitCount;\n    }\n\n    function isSunk() {\n        return (hitCount >= length);\n    }\n\n    function getHitCount() {\n        return hitCount;\n    }\n\n    return {\n        hit,\n        isSunk,\n        getHitCount\n    }\n};\n\n//# sourceURL=webpack:///./src/ship.js?");
+
+/***/ }),
+
 /***/ "./src/ui.js":
 /*!*******************!*\
   !*** ./src/ui.js ***!
   \*******************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"loadUI\": () => (/* binding */ loadUI)\n/* harmony export */ });\nfunction loadUI() {\n    const header = createHeader();\n    const mainContent = createMainContent();\n    const footer = createFooter();\n\n    document.body.append(header, mainContent, footer);\n}\n\nfunction createShipPlacementForm() {\n\n}\n\nfunction createHeader() {\n    const header = document.createElement(\"header\");\n    const heading = document.createElement(\"h1\");\n\n    heading.textContent = \"Battleship\";\n    header.appendChild(heading);\n    return header;\n}\n\nfunction createMainContent() {\n    const mainContent = document.createElement(\"section\");\n    mainContent.classList.add(\"main\");\n    let userBoard = createInitialBoard();\n    let enemyBoard = createInitialBoard();\n    mainContent.append(userBoard, enemyBoard);\n\n    return mainContent;\n}\n\nfunction createInitialBoard() {\n    let board = document.createElement(\"div\");\n    board.classList.add(\"board\");\n    \n    for (let x = 0; x < 10; ++x) {\n        let boardRow = document.createElement(\"div\");\n        boardRow.classList.add(\"board-row\");\n\n        for (let y = 0; y < 10; ++y) {\n            let square = document.createElement(\"div\");\n            square.classList.add(\"board-square\");\n            //TODO Make it so that clicking the enemy board's square attacks it\n            boardRow.appendChild(square);\n        }\n        board.appendChild(boardRow);\n    }\n    return board;\n}\n\nfunction renderBoard(boardToRender) {\n    \n}\n\nfunction createFooter() {\n    const footer = document.createElement(\"footer\");\n    const authorNote = document.createElement(\"p\");\n\n    authorNote.textContent = \"Copyright © 2022 Peepachu\";\n    footer.appendChild(authorNote);\n    return footer;\n}\n\n//# sourceURL=webpack:///./src/ui.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"loadUI\": () => (/* binding */ loadUI)\n/* harmony export */ });\n/* harmony import */ var _game__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./game */ \"./src/game.js\");\n\n\nfunction loadUI() {\n    const header = createHeader();\n    const mainContent = createMainContent();\n    const footer = createFooter();\n\n    document.body.append(header, mainContent, footer);\n}\n\nfunction createShipPlacementPopUp() {\n    const popUp = document.createElement(\"div\");\n    popUp.classList.add(\"popUp\");\n\n    const heading = document.createElement(\"h2\");\n    heading.textContent = \"Welcome to the battleship game\";\n\n    const info = document.createElement(\"span\");\n    info.textContent = \"Place your ships\";\n\n    const rotateButton = document.createElement(\"button\");\n    rotateButton.textContent = \"Rotate\";\n\n    popUp.append(heading, info, rotateButton);\n\n    return popUp;\n}\n\nfunction createHeader() {\n    const header = document.createElement(\"header\");\n    const heading = document.createElement(\"h1\");\n\n    heading.textContent = \"Battleship\";\n    header.appendChild(heading);\n    return header;\n}\n\nfunction createMainContent() {\n    const mainContent = document.createElement(\"section\");\n    mainContent.classList.add(\"main\");\n    let userBoard = createInitialBoard();\n    let enemyBoard = createInitialBoard();\n    renderBoard(_game__WEBPACK_IMPORTED_MODULE_0__.game.getEnemy().ownGameboard, enemyBoard);\n    mainContent.append(userBoard, enemyBoard);\n\n    return mainContent;\n}\n\nfunction createInitialBoard() {\n    let board = document.createElement(\"div\");\n    board.classList.add(\"board\");\n    \n    for (let x = 0; x < 10; ++x) {\n        let boardRow = document.createElement(\"div\");\n        boardRow.classList.add(\"board-row\");\n\n        for (let y = 0; y < 10; ++y) {\n            let square = document.createElement(\"div\");\n            square.classList.add(\"board-square\");\n            //TODO Make it so that clicking the enemy board's square attacks it\n            boardRow.appendChild(square);\n        }\n        board.appendChild(boardRow);\n    }\n    return board;\n}\n\nfunction renderBoard(boardToRender, boardOnScreen) {\n    const boardOnScreenRows = boardOnScreen.childNodes;\n    for (let x = 0; x < 10; ++x) {\n        const boardSquares = boardOnScreenRows[x].childNodes;\n        for (let y = 0; y < 10; ++y) {\n            if (boardToRender.hasShip([x, y]) != -1) {\n                if (boardToRender.hasBeenAttacked([x, y]))\n                    boardSquares[y].style.backgroundColor = \"red\";\n                else\n                    boardSquares[y].style.backgroundColor = \"grey\";\n            } else {\n                if (boardToRender.hasBeenAttacked([x, y]))\n                    boardSquares[y].style.backgroundColor = \"green\";\n            }\n        }\n    }\n}\n\nfunction createFooter() {\n    const footer = document.createElement(\"footer\");\n    const authorNote = document.createElement(\"p\");\n\n    authorNote.textContent = \"Copyright © 2022 Peepachu\";\n    footer.appendChild(authorNote);\n    return footer;\n}\n\n//# sourceURL=webpack:///./src/ui.js?");
 
 /***/ })
 
